fcce15679d568df61967581a1af4ca24acec0585
ParasRawat <parasmyname@gmail.com>
2020-05-02 14:35:33 +0530

    Updates

---
+++ precache-manifest.96cb3ef527237006afdc703d7fde31ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ceb27c084f87a8245fd51d9703354f",
+    "url": "/Capstone/index.html"
+  },
+  {
+    "revision": "63230e9886fc273fe096",
+    "url": "/Capstone/static/css/main.b9118a3d.chunk.css"
+  },
+  {
+    "revision": "f59f007b509c86ca92c7",
+    "url": "/Capstone/static/js/2.20c3f870.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Capstone/static/js/2.20c3f870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63230e9886fc273fe096",
+    "url": "/Capstone/static/js/main.ee7348a6.chunk.js"
+  },
+  {
+    "revision": "f418f52275a699c7222c",
+    "url": "/Capstone/static/js/runtime-main.3a413731.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Capstone/static/media/logo.5d5d9eef.svg"
+  }
+]);
